2e2d4fc98154ac3573e7549d96d188c008cea1d3
Andrew Zerdelian <121677421+AndrewZerdelian@users.noreply.github.com>
2024-03-25 16:29:15 +0200

    first commit

---
+++ App/_layouts.js
@@ -0,0 +1,7 @@
+import { stack } from "expo-router";
+
+function Layout() {
+  return <stack />;
+}
+
+export default Layout;
---
+++ App/index.js
@@ -0,0 +1,14 @@
+
+import { View, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Stack, useRouter } from "expo-router";
+
+function Home() {
+  return (
+    <View>
+      <Text>Home helloz ya Andrew </Text>
+    </View>
+  );
+}
+export default Home;
