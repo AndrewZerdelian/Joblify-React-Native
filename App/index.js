78420866f6df530026598c48acd5722f4205c79c
Andrew Zerdelian <121677421+AndrewZerdelian@users.noreply.github.com>
2024-03-25 16:45:23 +0200

    First Commit using GIT

--- App/index.js
+++ App/index.js
@@ -1,14 +1,22 @@
-
 import { View, Text, ScrollView } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack, useRouter } from "expo-router";
 
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
 function Home() {
+  const router = useRouter();
   return (
-    <View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Text>Home helloz ya Andrew </Text>
-    </View>
+    </SafeAreaView>
   );
 }
 export default Home;
